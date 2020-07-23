f743f926be4fcf09d6d58fcbb11808ad8a89a818
Abu Uzayr <abu.uzayr@builtforfifty.com>
2020-07-23 13:51:27 +0800

    Apply PT Sans as base font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   purge: ["./src/**/*.js", "./src/**/*.jsx", "./src/**/*.ts", "./src/**/*.tsx"],
-  theme: {},
+  theme: {
+    fontFamily: {
+      'body': ['PT Sans'],
+    }
+  },
   variants: {},
   plugins: [],
 }
