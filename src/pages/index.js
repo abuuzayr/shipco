c756be24247c61f98f04bf66fc9e4a5eaf138f8a
Abu Uzayr <abu.uzayr@builtforfifty.com>
2020-07-27 15:03:23 +0800

    Get data from prismic for hero text

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,43 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, StaticQuery, graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
+const heroQuery = graphql`
+  query {
+    prismic {
+      allHeros {
+        edges {
+          node {
+            title
+          }
+        }
+      }
+    }
+  }
+`;
+
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </Layout>
-)
+  <StaticQuery
+    query={heroQuery}
+    render={data => {
+      return (
+        <Layout>
+          <SEO title="Home" />
+          <h1 className="text-6xl text-blue-900 font-bold">
+            {data.prismic.allHeros.edges[0].node.title[0].text}
+          </h1>
+          <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+            <Image />
+          </div>
+          <Link to="/page-2/">Go to page 2</Link> <br />
+          <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+        </Layout>
+      )
+    }}
+  />
+);
 
 export default IndexPage
