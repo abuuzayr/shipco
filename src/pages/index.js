a5107898fe4896a3d8f53e0b204c944afc5035de
Abu Uzayr <abu.uzayr@builtforfifty.com>
2020-07-30 07:39:21 +0800

    Add projects query and component

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,6 +7,7 @@ import Hero from "../components/hero"
 import Intro from "../components/intro"
 import Work from "../components/work"
 import Approach from "../components/approach"
+import Projects from "../components/projects"
 
 const indexQuery = graphql`
   query {
@@ -60,6 +61,36 @@ const indexQuery = graphql`
                 url
               }
             }
+            project_section_title
+            project_button_text
+            project_button_url {
+              ... on PRISMIC__ExternalLink {
+                url
+              }
+              ... on PRISMIC__FileLink {
+                url
+              }
+              ... on PRISMIC__ImageLink {
+                url
+              }
+            }
+            projects {
+              project_description
+              project_tile_color
+              project_tile_width
+              project_title
+              project_url {
+                ... on PRISMIC__ExternalLink {
+                  url
+                }
+                ... on PRISMIC__FileLink {
+                  url
+                }
+                ... on PRISMIC__ImageLink {
+                  url
+                }
+              }
+            }
           }
         }
       }
@@ -83,6 +114,10 @@ const IndexPage = () => (
         approach_section_link_url,
         approach_section_text,
         approach_section_title,
+        project_section_title,
+        project_button_text,
+        project_button_url,
+        projects
       } = data.prismic.allHomepages.edges[0].node
       return (
         <Layout>
@@ -91,6 +126,7 @@ const IndexPage = () => (
           <Intro title={intro_section_title} text={intro_section_text} button={{ text: intro_button_text, url: intro_button_url }} />
           <Work title={work_section_title} nodes={work_experience} />
           <Approach title={approach_section_title} text={approach_section_text} button={{ text: approach_section_link_text, url: approach_section_link_url }} />
+          <Projects title={project_section_title} button={{ text: project_button_text, url: project_button_url }} nodes={projects} />
         </Layout>
       )
     }}
