5989838b5f48c513e138fb8e43a2b94da0264e37
Abu Uzayr <abu.uzayr@builtforfifty.com>
2020-07-30 16:44:33 +0800

    Set mobile for intro component

--- src/components/intro.js
+++ src/components/intro.js
@@ -4,13 +4,13 @@ import { Link } from "gatsby"
 import { RichText, Link as PrismicLink } from "prismic-reactjs"
 
 const Intro = ({ title, text, button }) => (
-    <div class="flex mb-4">
-        <div class="w-1/4">
+    <div class="md:flex mb-4">
+        <div class="md:w-1/4 w-full">
             <p className="font-bold text-blue-900 text-2xl">
                 <RichText render={title} />
             </p>
         </div>
-        <div class="w-3/4">
+        <div class="md:w-3/4 w-full">
             <p><RichText render={text} /></p>
             <div className="mt-10">
                 <Link to={button.url.url} className="font-bold rounded-full border border-blue-900 text-blue-900 px-6 py-3 hover:bg-blue-900 hover:text-white">
