7bf7e4ce73bd981fc1e8cf4b641ede0386d89dde
Abu Uzayr <abu.uzayr@builtforfifty.com>
2020-07-30 16:48:01 +0800

    Set mobile for approach component

--- src/components/approach.js
+++ src/components/approach.js
@@ -6,13 +6,13 @@ import { RichText, Link as PrismicLink } from "prismic-reactjs"
 const Approach = ({ title, text, button }) => {
     const url = PrismicLink.url(button.url)
     return (
-        <div class="flex my-40">
-            <div class="w-1/4">
+        <div class="md:flex my-40">
+            <div class="md:w-1/4 w-full">
                 <p className="font-bold text-blue-900 text-2xl">
                     {title[0].text}
                 </p>
             </div>
-            <div class="w-3/4">
+            <div class="md:w-3/4 w-full">
                 <p><RichText render={text} /></p>
                 <p className="mt-2">
                     <Link to={url} className="font-bold text-orange-600">
