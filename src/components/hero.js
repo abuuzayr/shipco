33e13da0e34db92050465bcacc3dc08e10ce59b6
Abu Uzayr <abu.uzayr@builtforfifty.com>
2020-12-04 17:51:07 +0800

    Update hero component for new prismic plugin

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,11 +1,10 @@
 import React from 'react'
 import PropTypes from "prop-types"
-import { RichText } from 'prismic-reactjs'
 
 const Hero = ({ text }) => (
-    <h1 className="text-4xl my-20 text-blue-900 leading-tight md:text-5xl md:my-40 font-black" style={{ fontFamily: "Merriweather" }}>
-        <RichText render={text} />
-    </h1>
+    <div className="text-4xl my-20 text-blue-900 leading-tight md:text-5xl md:my-40 font-black" style={{ fontFamily: "Merriweather" }}>
+        {text}
+    </div>
 )
 
 Hero.defaultProps = {
