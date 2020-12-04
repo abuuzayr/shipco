e0995e7b9deefcec4b9963c6c89e629a98795065
Abu Uzayr <abu.uzayr@builtforfifty.com>
2020-12-04 17:51:13 +0800

    Update intro component for new prismic plugin

--- src/components/intro.js
+++ src/components/intro.js
@@ -1,20 +1,19 @@
 import React from 'react'
 import PropTypes from "prop-types"
 import { Link } from "gatsby"
-import { RichText, Link as PrismicLink } from "prismic-reactjs"
 
 const Intro = ({ title, text, button }) => (
     <div class="md:flex mb-4">
         <div class="md:w-1/4 w-full">
             <p className="font-bold text-blue-900 text-xl">
-                <RichText render={title} />
+                {title}
             </p>
         </div>
         <div class="md:w-3/4 w-full">
-            <p><RichText render={text} /></p>
+            <p>{text}</p>
             <div className="mt-10">
-                <Link to={button.url.url} className="font-bold rounded-full border border-blue-900 text-blue-900 px-6 py-3 hover:bg-blue-900 hover:text-white">
-                    {button.text[0].text}
+                <Link to={button.url} className="font-bold rounded-full border border-blue-900 text-blue-900 px-6 py-3 hover:bg-blue-900 hover:text-white">
+                    {button.text}
                 </Link>
             </div>
         </div>
