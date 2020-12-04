4f0decd3a256ecd0af28a00ac07bdfa87a059c17
Abu Uzayr <abu.uzayr@builtforfifty.com>
2020-12-04 17:51:01 +0800

    Update approach component for new prismic plugin

--- src/components/approach.js
+++ src/components/approach.js
@@ -1,22 +1,20 @@
 import React from 'react'
 import PropTypes from "prop-types"
 import { Link } from "gatsby"
-import { RichText, Link as PrismicLink } from "prismic-reactjs"
 
 const Approach = ({ title, text, button }) => {
-    const url = PrismicLink.url(button.url)
     return (
         <div class="md:flex my-40">
             <div class="md:w-1/4 w-full">
                 <p className="font-bold text-blue-900 text-xl">
-                    {title[0].text}
+                    {title}
                 </p>
             </div>
             <div class="md:w-3/4 w-full">
-                <p><RichText render={text} /></p>
-                <p className="mt-2">
-                    <Link to={url} className="font-bold text-orange-600">
-                        {button.text[0].text}
+                <p>{text}</p>
+                <p className="mt-4">
+                    <Link to={button.url} className="font-bold text-orange-600 text-sm">
+                        {button.text}
                     </Link>
                 </p>
             </div>
