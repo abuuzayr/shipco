97af57d4447ab15b700a3282207f3885dec9e98a
Abu Uzayr <abu.uzayr@builtforfifty.com>
2020-12-04 17:51:33 +0800

    Update profile component for new prismic plugin

--- src/components/profile.js
+++ src/components/profile.js
@@ -1,54 +1,41 @@
 import React from "react"
-import { StaticQuery, graphql, Link } from "gatsby"
-import { RichText } from "prismic-reactjs"
+import { StaticQuery, graphql } from "gatsby"
 import { FaInstagram, FaLinkedinIn, FaEnvelope } from "react-icons/fa"
 
 const query = graphql`
   query {
-    prismic {
-      allProfiles {
-        edges {
-          node {
-            name
-            position
-            resume {
-              ... on PRISMIC__ExternalLink {
-                url
-              }
-              ... on PRISMIC__FileLink {
-                url
-              }
-              ... on PRISMIC__ImageLink {
-                url
-              }
-            }
-            resume_button_text
-            linkedin_url {
-              ... on PRISMIC__ExternalLink {
-                url
-              }
-              ... on PRISMIC__FileLink {
-                url
-              }
-              ... on PRISMIC__ImageLink {
-                url
-              }
-            }
-            linkedin_text
-            instagram_url {
-              ... on PRISMIC__ExternalLink {
-                url
-              }
-              ... on PRISMIC__FileLink {
-                url
-              }
-              ... on PRISMIC__ImageLink {
-                url
-              }
-            }
-            instagram_text
-            email
-            description
+    allPrismicProfile {
+      nodes {
+        data {
+          name {
+            text
+          }
+          position {
+            text
+          }
+          resume {
+            url
+          }
+          resume_button_text {
+            text
+          }
+          linkedin_url {
+            url
+          }
+          linkedin_text {
+            text
+          }
+          instagram_url {
+            url
+          }
+          instagram_text {
+            text
+          }
+          email {
+            text
+          }
+          description {
+            text
           }
         }
       }
@@ -71,25 +58,25 @@ const Profile = () => (
         instagram_text,
         email,
         description,
-      } = data.prismic.allProfiles.edges[0].node
+      } = data.allPrismicProfile.nodes[0].data
       return (
         <div class="md:flex my-40">
           <div class="md:w-1/4 w-0"></div>
           <div class="md:w-3/4 w-full">
             <div className="mb-8">
               <h2 className="font-bold text-blue-900 text-xl flex items-center">
-                {name[0].text}
+                {name.text}
               </h2>
               <p className="font-bold my-4">
-                {position[0].text}
+                {position.text}
               </p>
-              <p><RichText render={description} /></p>
+              <p>{description.text}</p>
             </div>
             {
               linkedin_url && linkedin_text &&
               <p className="mb-2">
                 <a href={linkedin_url.url} className="flex items-center text-orange-600" target="_blank">
-                  <FaLinkedinIn className="mr-2" /> {linkedin_text[0].text}
+                  <FaLinkedinIn className="mr-2" /> {linkedin_text.text}
                 </a>
               </p>
             }
@@ -97,15 +84,15 @@ const Profile = () => (
               instagram_url && instagram_text &&
               <p className="mb-2">
                 <a href={instagram_url.url} className="flex items-center text-orange-600" target="_blank">
-                  <FaInstagram className="mr-2" /> {instagram_text[0].text}
+                  <FaInstagram className="mr-2" /> {instagram_text.text}
                 </a>
               </p>
             }
             {
               email &&
               <p className="mb-2">
-                <a href={`mailto:${email[0].text}`} className="flex items-center text-orange-600" target="_blank">
-                  <FaEnvelope className="mr-2" /> {email[0].text}
+                <a href={`mailto:${email.text}`} className="flex items-center text-orange-600" target="_blank">
+                  <FaEnvelope className="mr-2" /> {email.text}
                 </a>
               </p>
             }
@@ -113,7 +100,7 @@ const Profile = () => (
               resume_button_text && resume &&
               <p className="mt-10">
                 <a href={resume.url} className="font-bold rounded-full border border-blue-900 text-blue-900 px-6 py-3 hover:bg-blue-900 hover:text-white" target="_blank">
-                  {resume_button_text[0].text}
+                  {resume_button_text.text}
                 </a>
               </p>
             }
