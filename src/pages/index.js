cc1dc818d7ce21b5bb7efc12a5b23552aba718dd
Abu Uzayr <abu.uzayr@builtforfifty.com>
2020-12-05 20:14:48 +0800

    Update index to use new graphql plugin

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,92 +12,67 @@ import Profile from "../components/profile"
 
 const indexQuery = graphql`
   query {
-    prismic {
-      allHomepages {
-        edges {
-          node {
-            hero_text
-            intro_section_title
-            intro_section_text
-            intro_button_text
-            intro_button_url {
-              ... on PRISMIC__ExternalLink {
-                url
-              }
-              ... on PRISMIC__FileLink {
-                url
-              }
-              ... on PRISMIC__ImageLink {
-                url
-              }
+    allPrismicHomepage {
+      nodes {
+        data {
+          hero_text {
+            text
+          }
+          intro_section_title {
+            text
+          }
+          intro_section_text {
+            text
+          }
+          intro_button_text {
+            text
+          }
+          intro_button_url {
+            url
+          }
+          work_section_title {
+            text
+          }
+          work_experience {
+            company {
+              text
             }
-            work_section_title
-            work_experience {
-              company
-              duration
-              position
-              url {
-                ... on PRISMIC__ExternalLink {
-                  url
-                }
-                ... on PRISMIC__FileLink {
-                  url
-                }
-                ... on PRISMIC__ImageLink {
-                  url
-                }
-              }
+            duration {
+              text
             }
-            approach_section_link_text
-            approach_section_text
-            approach_section_title
-            approach_section_link_url {
-              ... on PRISMIC__ExternalLink {
-                url
-              }
-              ... on PRISMIC__FileLink {
-                url
-              }
-              ... on PRISMIC__ImageLink {
-                url
-              }
+            position {
+              text
             }
-            project_section_title
-            project_button_text
-            project_button_url {
-              ... on PRISMIC__ExternalLink {
-                url
-              }
-              ... on PRISMIC__FileLink {
-                url
-              }
-              ... on PRISMIC__ImageLink {
-                url
-              }
+            long_description {
+              text
             }
-            projects {
-              project_description
-              project_tile_color
-              project_tile_width
-              project_title
-              project_url {
-                ... on PRISMIC__ExternalLink {
-                  url
-                }
-                ... on PRISMIC__FileLink {
-                  url
-                }
-                ... on PRISMIC__ImageLink {
-                  url
-                }
-              }
-            }
+          }
+          approach_section_link_text {
+            text
+          }
+          approach_section_text {
+            text
+          }
+          approach_section_title {
+            text
+          }
+          approach_section_link_url {
+            url
+          }
+          project_section_title {
+            text
+          }
+          project_button_text {
+            text
+          }
+          project_button_url {
+            url
           }
         }
       }
     }
   }
-`;
+`
 
 const IndexPage = () => (
   <StaticQuery
@@ -117,17 +92,32 @@ const IndexPage = () => (
         approach_section_title,
         project_section_title,
         project_button_text,
-        project_button_url,
-        projects
-      } = data.prismic.allHomepages.edges[0].node
+        project_button_url
+      } = data.allPrismicHomepage.nodes[0].data
       return (
         <Layout>
           <SEO title="Home" />
-          <Hero text={hero_text} />
-          <Intro title={intro_section_title} text={intro_section_text} button={{ text: intro_button_text, url: intro_button_url }} />
-          <Work title={work_section_title} nodes={work_experience} />
-          <Approach title={approach_section_title} text={approach_section_text} button={{ text: approach_section_link_text, url: approach_section_link_url }} />
-          <Projects title={project_section_title} button={{ text: project_button_text, url: project_button_url }} nodes={projects} />
+          <Hero text={hero_text.text} />
+          <Intro
+            title={intro_section_title.text}
+            text={intro_section_text.text}
+            button={{ text: intro_button_text.text, url: intro_button_url.url }}
+          />
+          <Work title={work_section_title.text} nodes={work_experience} />
+          <Approach
+            title={approach_section_title.text}
+            text={approach_section_text.text}
+            button={{
+              text: approach_section_link_text.text,
+              url: approach_section_link_url.url,
+            }}
+          />
+          <Projects
+            tilesMode="dynamic"
+            title={project_section_title.text}
+            btnText={project_button_text.text}
+            btnUrl={project_button_url.url}
+          />
           <Profile />
         </Layout>
       )
