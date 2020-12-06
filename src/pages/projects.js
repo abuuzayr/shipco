35528daae4bcdbe5fa08c6177816661674b6de01
Abu Uzayr <abu.uzayr@builtforfifty.com>
2020-12-06 11:40:57 +0800

    Add projects page

---
+++ src/pages/projects.js
@@ -0,0 +1,20 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import Projects from "../components/projects"
+
+const ProjectsPage = () => (
+  <Layout>
+    <SEO title="Projects" />
+    <h1
+      style={{ fontFamily: "Merriweather", color: "#062D5B" }}
+      className="mb-20 text-5xl"
+    >
+      Projects
+    </h1>
+    <Projects />
+  </Layout>
+)
+
+export default ProjectsPage
