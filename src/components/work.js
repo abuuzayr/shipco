207fbde52a59a2febf120341df35b56c73135008
Abu Uzayr <abu.uzayr@builtforfifty.com>
2020-12-06 10:59:52 +0800

    Add active indicators for work

--- src/components/work.js
+++ src/components/work.js
@@ -19,62 +19,72 @@ const transitionStyles = {
 const Work = ({ title, nodes }) => {
     const [active, setActive] = useState(0)
     return (
-        <div class="md:flex my-40">
-            <div class="md:w-1/4 w-full">
-                <p className="font-bold text-blue-900 text-xl">
-                    {title}
-                </p>
-            </div>
-            <div class="md:w-3/4 w-full">
-                {
-                    nodes.map((n, index) => {
-                        const { company, position, duration, long_description } = n
-                        return (
-                          <div className="mb-8">
-                            <div
-                              className="font-bold text-blue-900 text-xl flex items-center cursor-pointer"
-                              onClick={() => setActive(active === index ? null : index)}
-                            >
-                              {company.text}{" "}
-                              {active !== index && (
-                                <IoIosArrowForward className="text-blue-900 ml-2" />
-                              )}
-                            </div>
-                            <p className="font-bold text-blue-900 mt-6">
-                              {position.text}
-                            </p>
-                            <p className="italic">{duration.text}</p>
-                            <Transition in={active === index} timeout={300}>
-                              {state => (
-                                <div
-                                  style={{
-                                    ...defaultStyle,
-                                    ...transitionStyles[state],
-                                  }}
-                                >
-                                  <p
-                                    dangerouslySetInnerHTML={{
-                                      __html: long_description.html,
-                                    }}
-                                    className="html-block"
-                                  ></p>
-                                  <button
-                                    className="mt-6 font-bold text-sm"
-                                    style={{ color: "#D74000" }}
-                                    onClick={() => setActive(null)}
-                                  >
-                                    Hide
-                                  </button>
-                                </div>
-                              )}
-                            </Transition>
-                          </div>
-                        )
-                    })
-                }
-            </div>
-            <style jsx></style>
+      <div class="md:flex my-40">
+        <div class="md:w-1/4 w-full">
+          <p className="font-bold text-blue-900 text-xl">{title}</p>
         </div>
+        <div class="md:w-3/4 w-full pl-8 border-l-2 border-dashed">
+          {nodes.map((n, index) => {
+            const { company, position, duration, long_description } = n
+            return (
+              <div
+                className={`relative ${
+                  index < nodes.length - 1
+                    ? "mb-8"
+                    : "-ml-10 pl-8 border-l-8 border-white"
+                }`}
+              >
+                <div
+                  className="w-4 h-4 rounded-full absolute -ml-10 mt-1"
+                  style={{ background: "#DEE5EE" }}
+                ></div>
+                <div
+                  className="w-2 h-2 rounded-full bg-gray-400 absolute -ml-9 mt-2"
+                  style={{
+                    background: active === index ? "#062D5B" : "#BDBDBD",
+                  }}
+                ></div>
+                <div
+                  className="font-bold text-blue-900 text-xl flex items-center cursor-pointer -mt-1"
+                  onClick={() => setActive(active === index ? null : index)}
+                >
+                  {company.text}{" "}
+                  {active !== index && (
+                    <IoIosArrowForward className="text-blue-900 ml-2" />
+                  )}
+                </div>
+                <p className="font-bold text-blue-900 mt-6">{position.text}</p>
+                <p className="italic">{duration.text}</p>
+                <Transition in={active === index} timeout={300}>
+                  {state => (
+                    <div
+                      style={{
+                        ...defaultStyle,
+                        ...transitionStyles[state],
+                      }}
+                    >
+                      <p
+                        dangerouslySetInnerHTML={{
+                          __html: long_description.html,
+                        }}
+                        className="html-block"
+                      ></p>
+                      <button
+                        className="mt-6 font-bold text-sm"
+                        style={{ color: "#D74000" }}
+                        onClick={() => setActive(null)}
+                      >
+                        Hide
+                      </button>
+                    </div>
+                  )}
+                </Transition>
+              </div>
+            )
+          })}
+        </div>
+        <style jsx></style>
+      </div>
     )
 }
 
