d672d0b0d9fab53060c4759175e577e68043a549
Abu Uzayr <abu.uzayr@builtforfifty.com>
2020-12-10 15:13:38 +0800

    Handle if URL is external

--- src/components/intro.js
+++ src/components/intro.js
@@ -3,21 +3,32 @@ import PropTypes from "prop-types"
 import { Link } from "gatsby"
 
 const Intro = ({ title, text, button }) => (
-  <div class="md:flex mb-4">
+  <div class="md:flex mb-4" id="intro">
     <div class="mb-10 md:mb-0 md:w-1/4 w-full">
       <p className="font-bold text-blue-900 text-xl">{title}</p>
     </div>
     <div class="md:w-3/4 w-full">
       <div class="html-block">
-        <div className="-mt-4" dangerouslySetInnerHTML={{ __html: text }}/>
+        <div className="-mt-4" dangerouslySetInnerHTML={{ __html: text }} />
       </div>
       <div className="mt-16">
-        <Link
-          to={button.url}
-          className="font-bold rounded-full border border-blue-900 text-blue-900 px-6 py-3 hover:bg-blue-900 hover:text-white"
-        >
-          {button.text}
-        </Link>
+        {button.url && button.url.includes("http") ? (
+          <a
+            href={button.url}
+            className="font-bold rounded-full border border-blue-900 text-blue-900 px-6 py-3 hover:bg-blue-900 hover:text-white"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {button.text}
+          </a>
+        ) : (
+          <Link
+            to={button.url || "#projects"}
+            className="font-bold rounded-full border border-blue-900 text-blue-900 px-6 py-3 hover:bg-blue-900 hover:text-white"
+          >
+            {button.text}
+          </Link>
+        )}
       </div>
     </div>
   </div>
