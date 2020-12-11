42144db23983ca2d46ffbbef359fa87044e18c6f
Abu Uzayr <abu.uzayr@builtforfifty.com>
2020-12-11 10:46:42 +0800

    Update styling for hero text

--- src/components/hero.js
+++ src/components/hero.js
@@ -2,7 +2,7 @@ import React from 'react'
 import PropTypes from "prop-types"
 
 const Hero = ({ text }) => (
-    <div className="text-4xl my-20 text-blue-900 leading-tight md:text-5xl md:my-40 font-black" style={{ fontFamily: "Merriweather" }} id="hero">
+    <div className="text-3xl my-20 text-blue-900 leading-snug md:leading-snug md:text-5xl md:my-40 font-black" style={{ fontFamily: "Merriweather", letterSpacing: "-0.01em" }} id="hero">
         {text}
     </div>
 )
