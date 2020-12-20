a343a4ad1a5f4a5945273debe8b08790b737ec6f
Abu Uzayr <abu.uzayr@builtforfifty.com>
2020-12-20 20:22:13 +0800

    Adjust margins, sizes for new chat paddings

--- src/components/chat.js
+++ src/components/chat.js
@@ -123,22 +123,22 @@ const Chat = ({ setOverlay }) => {
     return (
         <div className="flex items-center justify-center min-h-screen px-4 text-center sm:block sm:p-0" ref={chatContainer}>
             <span className="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
-            <div className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform sm:my-8 sm:align-middle sm:w-full" role="dialog" aria-modal="true" aria-labelledby="modal-headline" style={{ height: 570, maxWidth: 608 }}>
+            <div className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform sm:my-8 sm:align-middle sm:w-full" role="dialog" aria-modal="true" aria-labelledby="modal-headline" style={{ height: 546, maxWidth: 608 }}>
                 <div className="shadow-2xl p-1 rounded-full float-right mt-4 mr-4 cursor-pointer border box-border" style={{
                     borderColor: "#F6F6F6",
                     boxShadow: "0px 0px 5px rgba(0, 0, 0, 0.1)",
                 }} onClick={() => setOverlay(false)}>
-                    <IoClose size={10} />
+                    <IoClose size={14} />
                 </div>
-                <div className="bg-white px-4 pt-5 pb-4 sm:p-20 sm:pb-4">
+                <div className="bg-white px-4 pt-5 pb-4 sm:p-20 sm:pb-6">
                     <div className="font-bold text-2xl pb-4" style={{ color: "#062D5B" }}>Work with me</div>
                     <div>You can always email or contact me on my social platforms. Since you are already here, just send a message below!</div>
                 </div>
                 <div ref={messageContainer} className="px-4 sm:px-20 overflow-y-scroll chat-inner-container">
-                    {messages.map(message => {
+                    {messages.map((message, index) => {
                         const commonClasses = "rounded-t-2xl p-6 max-w-xs mb-2 clear-both"
                         return (
-                            <div>
+                            <div className={index === messages.length - 1 ? `mb-6` : ''}>
                                 {message.from === 'user' ? (
                                     <div className={`${commonClasses} rounded-bl-2xl float-right`} style={{ background: "#EEEEEE" }}>
                                         {message.body}
@@ -152,7 +152,7 @@ const Chat = ({ setOverlay }) => {
                         )
                     })}
                 </div>
-                <div className="bottom-0 fixed w-full p-4 border-t flex">
+                <div className="bottom-0 fixed w-full p-4 sm:p-6 border-t flex">
                     {stage < stages.length - 1 ? 
                         <>
                             <input className="rounded-l-full w-full px-5 py-2 outline-none" type="text" placeholder="Enter your message" style={{ background: "#F6F6F6" }} value={message} onChange={handleKeyUp} onKeyUp={handleKeyUp} ref={messageInput} />
