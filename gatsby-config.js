8b51cb4f5b3d7d8643d291774f06ea4e8a47ee9f
shipengng <68346134+shipengng@users.noreply.github.com>
2021-01-02 04:48:25 +0800

    Update gatsby-config.js
    Updated GA tracking ID

--- gatsby-config.js
+++ gatsby-config.js
@@ -54,7 +54,7 @@ module.exports = {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         // The property ID; the tracking code won't be generated without it
-        trackingId: "UA-111111111-1",
+        trackingId: "UA-30335104-1",
         head: false,
       },
     },
